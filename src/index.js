fb146b012ec3723109dc7defcd30e20a2e9b1062
ZPitor99 <gabriel.chifflet7845@outlook.fr>
2026-03-07 00:53:25 +0100

    index deb

--- src/index.js
+++ src/index.js
@@ -3,3 +3,9 @@ import Alpine from 'alpinejs'
 window.Alpine = Alpine
 
 Alpine.start()
+
+import '@fontsource/sora/300.css';
+import '@fontsource/sora/400.css';
+import '@fontsource/sora/600.css';
+import '@fontsource/lora/400.css';
+import '@fontsource/lora/600.css';
