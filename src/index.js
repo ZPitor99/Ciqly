e2a1197bccdeb5cea480541daf40018f2772502b
ZPitor99 <gabriel.chifflet7845@outlook.fr>
2026-03-08 00:52:04 +0100

    Mise en place style commun

--- src/index.js
+++ src/index.js
@@ -6,6 +6,12 @@ Alpine.start()
 
 import '@fontsource/sora/300.css';
 import '@fontsource/sora/400.css';
+import '@fontsource/sora/500.css';
 import '@fontsource/sora/600.css';
+import '@fontsource/sora/700.css';
 import '@fontsource/lora/400.css';
 import '@fontsource/lora/600.css';
+import '@fontsource/lora/400-italic.css';
+
+import './css/style_commun.css'
+import './css/style_index.css'
